b57a160d395d014b7cbde09156890296dbb38cb8
Ghostnooob123 <hero.zerodoku@gmail.com>
2024-01-27 23:39:22 +0200

    Added better frontend( register user, reset users, get users), updated backend(better put request)

--- src/main/java/frontend/src/App.vue
+++ src/main/java/frontend/src/App.vue
@@ -17,7 +17,7 @@
       <tr v-for="account in accounts" :key="account.accountHolder_uniqueId">
         <td>
           <button @click="editAccount(account)">
-            Edit
+            Edit Confirm
           </button>
         </td>
         <td>
@@ -34,11 +34,44 @@
 
     <p></p>
 
+    <p v-if="newRegister > 0">
+      Reset the table
+    </p>
+    <p v-else-if="newRegister < 0">
+      Up-To-Date
+    </p>
+
     <button @click="reset"> Reset </button>
 
     <p></p>
 
-    <button @click="register"> Register</button>
+    <button @click="register"> Register </button>
+    <table style="width:100%">
+      <tr>
+        <th>Account Name</th>
+        <th>Account Credit_Debit_Card</th>
+        <th>Account Balance</th>
+        <th>Account Date of birth</th>
+      </tr>
+      <tr>
+        <td>
+          <input v-model="this.name" type="text">
+        </td>
+        <td>
+          <input v-model="this.card" type="text">
+        </td>
+        <td>
+          <input v-model="this.balance" type="text">
+        </td>
+        <td>
+          <input v-model="this.dob" type="date">
+        </td>
+      </tr>
+    </table>
+
+    <p/>
+
+    <p>{{this.registerFailed}}</p>
   </div>
 
 </template>
@@ -53,6 +86,13 @@ export  default {
     return {
       accounts: null,
       buttonClicked: 0,
+      newRegister: 0,
+      registerFailed:"",
+      name: "",
+      card: "",
+      balance: 0.00,
+      status: "Active",
+      dob: null,
     }
   },
   methods: {
@@ -90,23 +130,38 @@ export  default {
         const response = await axios.get('http://localhost:8082/api/v1/bank'); // replace with your actual API endpoint
         this.accounts = response.data;
 
+        this.newRegister = -1;
+
         console.log(response)
       } catch (error) {
-        console.error('Error fetching accounts:', error);
+        console.error('Error reset accounts:', error);
       }
     },
     async register() {
       try {
-        const response = await axios.post(
-            'http://localhost:8082/api/v1/bank',
-            {
-              accountHolder_Balance: Number(111),
-              accountHolder_Name: 'Ivo'
-            });
+        let response = null;
+
+        if (this.name != "" && this.card != "" && this.dob != null){
+          response = await axios.post(
+              'http://localhost:8082/api/v1/bank',
+              {
+                accountHolder_Name: this.name,
+                accountHolder_Credit_Debit_Card: this.card,
+                accountHolder_Balance: this.balance,
+                accountHolder_Status: this.status,
+                accountHolder_Dob: this.dob,
+              });
+
+          this.newRegister = 1;
+          this.registerFailed = "";
+        }
+        else {
+          this.registerFailed = "Fields need to be filled: Name, Card, Dob correctly.";
+        }
 
         console.log(response);
       } catch (error) {
-        console.error('Error editing account:', error);
+        console.error('Error register account:', error);
       }
     }
   }
